990868b16d02db46b7d36512da1f959ef324eee1
peterluy-UML <peter_luy@student.uml.edu>
2024-03-03 17:09:49 -0500

    first commit

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,12 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* Calculator Application that returns the sum of two integers.
+*/
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
+        final static int UML_NUMBER_1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
